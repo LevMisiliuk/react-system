66976f023d2da0518c6c79c182d1a3b49195f9d3
Dmitrij <d.kamkov@splinestudio.ua>
2023-04-13 20:17:50 +0300

    strps style

--- client/src/pages/StepsPage.jsx
+++ client/src/pages/StepsPage.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Header from 'components/header/Header'
-import Sidebar from 'components/sidebar/Sidebar'
 import StepsPolicy from 'components/steps/StepsPolicy'
 
 function StepsPage() {
   return (
     <>
       <Header />
-      <Sidebar />
-      <div className="container">
+      <div className="container-steps">
         <StepsPolicy />
       </div>
     </>
